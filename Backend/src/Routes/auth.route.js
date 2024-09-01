125d2e04023b7f1cd580931a3ae514c126ab3708
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2024-09-01 19:23:40 +0530

    route created

--- Backend/src/Routes/auth.route.js
+++ Backend/src/Routes/auth.route.js
@@ -1,10 +1,12 @@
 const express= require("express");
 const userController = require("../Controller/user.controller");
 const { CREATE_USER } = require("../constant/user.constant");
+const { CREATE_TEACHER } = require("../constant/teacher.constant");
+const TeacherController = require("../Controller/teacher.controller");
 const router= express.Router()
 
 const usercontroller= new userController()
-
+const teachercontroller= new TeacherController
 router.post(CREATE_USER,usercontroller.createUser);
-
+router.post(CREATE_TEACHER,teachercontroller.createUserTeacher)
 module.exports=router
