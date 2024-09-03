3d51a628d34e9e4a063e3ff3d7ee23b9d7abf1a9
ishikavats52 <sishika077@gmail.com>
2024-09-03 22:48:50 +0530

    teacher,manager and student dash created

---
+++ signgo/src/app/student/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Studentdash } from '@/components/component/Studentdash'
+function page() {
+  return (
+    <div>
+      <Studentdash/>
+    </div>
+  )
+}
+
+export default page
---
+++ signgo/src/app/teacher/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Teacherdash } from '@/components/component/Teacherdash'
+function page() {
+  return (
+    <div>
+      <Teacherdash/>
+    </div>
+  )
+}
+
+export default page
