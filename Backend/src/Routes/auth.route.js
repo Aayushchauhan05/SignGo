bb323601cff198589c9763e3f573ecdbdeb0dd57
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2024-09-03 19:28:58 +0530

    backend error resolve

--- Backend/src/Routes/auth.route.js
+++ Backend/src/Routes/auth.route.js
@@ -1,16 +1,18 @@
-const express= require("express");
+const express = require("express");
 const userController = require("../Controller/user.controller");
 const { CREATE_USER, LOGIN_USER } = require("../constant/user.constant");
 const { CREATE_TEACHER, LOGIN_TEACHER } = require("../constant/teacher.constant");
 const TeacherController = require("../Controller/teacher.controller");
-const router= express.Router()
+const router = express.Router();
 
-const usercontroller= new userController
-const teachercontroller= new TeacherController
-router.post(CREATE_USER,usercontroller.createUser);
-router.post(CREATE_TEACHER,teachercontroller.createUserTeacher)
+const usercontroller = new userController();
+const teachercontroller = new TeacherController();
 
-// login
-router.post(LOGIN_USER,usercontroller.loginUser)
-router.post(LOGIN_TEACHER,teachercontroller.loginTeacher)
-module.exports=router
+router.post(CREATE_USER, usercontroller.createUser);
+router.post(CREATE_TEACHER, teachercontroller.createUserTeacher);
+
+// Login routes
+router.post(LOGIN_USER, usercontroller.loginUser);
+router.post(LOGIN_TEACHER, teachercontroller.loginTeacher);
+
+module.exports = router;
